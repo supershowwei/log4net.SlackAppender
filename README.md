bbd979d0af12145320eb0e5cdcb88ac63e0ef3af
johnny <johnny@wantgoo.com>
2016-10-21 12:00:33 +0800

    complete

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # log4net.SlackAppender
+
+Send log to Slack.
+
+### log4net config
+
+```xml
+<log4net>
+  <appender name="SlackAppender" type="log4net.Appender.SlackAppender, log4net.SlackAppender">
+    <webhookUrl>{Your WebhookUrl}</webhookUrl>
+    <layout type="log4net.Layout.PatternLayout">
+      <conversionPattern value="%type{1}.%method%newline%message" />
+    </layout>
+    <filter type="log4net.Filter.LevelRangeFilter">
+      <levelMin value="WARN"/>
+      <levelMax value="ERROR"/>
+    </filter>
+  </appender>
+  <root>
+    <level value="INFO"/>
+    <appender-ref ref="SlackAppender"/>
+  </root>
+</log4net>
+```
---
+++ log4net.SlackAppender/log4net.SlackAppender/SlackAppender.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using log4net.Core;
+using log4net.SlackAppender;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RestSharp;
+
+namespace log4net.Appender
+{
+    public class SlackAppender : AppenderSkeleton
+    {
+        public string WebhookUrl { get; set; }
+
+        protected override async void Append(LoggingEvent loggingEvent)
+        {
+            var client = new RestClient(this.WebhookUrl);
+
+            var request = new RestRequest(Method.POST);
+
+            var payload = this.GeneratePayload(loggingEvent);
+
+            request.AddParameter(
+                "payload",
+                JsonConvert.SerializeObject(
+                    payload,
+                    new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }));
+
+            await client.ExecuteTaskAsync(request);
+        }
+
+        private static string GetColor(Level level)
+        {
+            switch (level.DisplayName.ToLowerInvariant())
+            {
+                case "warn":
+                    return "warning";
+                case "error":
+                case "fatal":
+                    return "danger";
+                default:
+                    return "good";
+            }
+        }
+
+        private Payload GeneratePayload(LoggingEvent loggingEvent)
+        {
+            return new Payload
+                       {
+                           Username =
+                               string.Format(
+                                   "{0} - {1}",
+                                   GlobalContext.Properties["ApplicationName"],
+                                   loggingEvent.LoggerName),
+                           Fallback = string.Format("Occurs {0}", loggingEvent.Level.DisplayName),
+                           Color = GetColor(loggingEvent.Level),
+                           Fields =
+                               new List<Field>
+                                   {
+                                       new Field
+                                           {
+                                               Title = loggingEvent.Level.DisplayName,
+                                               Value = this.RenderLoggingEvent(loggingEvent),
+                                               Short = false
+                                           }
+                                   }
+                       };
+        }
+    }
+}
