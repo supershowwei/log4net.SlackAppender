afe8a313694b52213c7a3a2d7644fa5486d60a41
johnny <johnny@wantgoo.com>
2016-10-24 16:24:08 +0800

    add Field.cs, Payload.cs update .gitignore

---
+++ log4net.SlackAppender/log4net.SlackAppender/Field.cs
@@ -0,0 +1,11 @@
+﻿namespace log4net.SlackAppender
+{
+    internal class Field
+    {
+        public bool Short { get; set; }
+
+        public string Title { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ log4net.SlackAppender/log4net.SlackAppender/Payload.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using log4net.SlackAppender;
+
+namespace log4net.Appender
+{
+    internal class Payload
+    {
+        public string Color { get; set; }
+
+        public string Fallback { get; set; }
+
+        public List<Field> Fields { get; set; }
+
+        public string Username { get; set; }
+    }
+}
